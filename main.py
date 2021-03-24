b8917fa55e7e2a624a0c33ae76d9b0fbefc362b7
canto9112 <cantoweb112@gmail.com>
2021-03-24 19:08:52 +0300

    Новая функция - загрузка картинки в альбом группы

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ def push_text_vk(token, text, owner_id):
     vk_session = vk_api.VkApi(token=token)
 
     vk = vk_session.get_api()
-    vk.wall.post(message=text, owner_id=owner_id, from_group=1)
+    vk.wall.post(message=text, owner_id=-owner_id, from_group=1)
+
+
+def upload_image(token, photo, album_id, owner_id):
+    vk_session = vk_api.VkApi(token=token)
+    upload = vk_api.VkUpload(vk_session)
+
+    upload.photo(photo, album_id=album_id, group_id=owner_id)
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    vk_token = env('VK_TOKEN')
+    vk_token = env('VK_USER_TOKEN')
+
     test_message = 'hello world'
-    owner_id = '-203517016'
+    owner_id = '203517016'
+    album_id = '280071703'
+    photo = 'test_photo.jpg'
     from_group = 1  # 1 — запись будет опубликована от имени группы, 0 — запись будет опубликована от имени пользователя
-    push_text_vk(vk_token, test_message, owner_id)
+
+    # push_text_vk(vk_token, test_message, owner_id)
+    upload_image(vk_token, photo, album_id, owner_id)
