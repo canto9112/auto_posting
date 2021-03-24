19e94084a04ed66973dddf5402ce368a9f7f02dc
canto9112 <cantoweb112@gmail.com>
2021-03-24 19:58:43 +0300

    Новая функция - отправка поста (текст + картинка) в канал телеграм

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,12 @@
 import telegram
 from environs import Env
 
+
+def upload_post_to_chanal(bot, chanal_name, text, image):
+    bot.send_photo(chat_id=chanal_name, photo=open(image, 'rb'))
+    bot.send_message(chat_id=chanal_name, text=text)
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
@@ -11,6 +17,6 @@ if __name__ == '__main__':
 
     chanal_name = '@auto_posting_vk_tg'
     test_message = 'hello world'
+    photo = 'test_photo.jpg'
 
-    bot.send_message(chat_id=chanal_name, text=test_message)
-    print(bot.get_me())
+    upload_post_to_chanal(bot, chanal_name, test_message, photo)
