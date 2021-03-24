0414b8ea75d2c74c7767d9f7c14b2d979426d310
canto9112 <cantoweb112@gmail.com>
2021-03-24 19:29:09 +0300

    Новая функция - загрузка поста (текст + картинка) на стену

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from environs import Env
 import vk_api
+from pprint import pprint
 
 
-def push_text_vk(token, text, owner_id):
-    vk_session = vk_api.VkApi(token=token)
-
-    vk = vk_session.get_api()
-    vk.wall.post(message=text, owner_id=-owner_id, from_group=1)
-
-
-def upload_image(token, photo, album_id, owner_id):
+def upload_image_alboum(token, photo, album_id, owner_id):
     vk_session = vk_api.VkApi(token=token)
     upload = vk_api.VkUpload(vk_session)
 
-    upload.photo(photo, album_id=album_id, group_id=owner_id)
+    photo_img = upload.photo(photo,
+                             album_id=album_id,
+                             group_id=owner_id)
+    image_id = photo_img[0]['id']
+    return image_id
+
+
+def upload_post_to_wall(token, text, owner_id, media_id):
+    vk_session = vk_api.VkApi(token=token)
+
+    vk = vk_session.get_api()
+    vk.wall.post(message=text,
+                 owner_id=-owner_id,
+                 from_group=1,
+                 attachments=f'photo{-owner_id}_{media_id}')
 
 
 if __name__ == '__main__':
@@ -23,10 +31,10 @@ if __name__ == '__main__':
     vk_token = env('VK_USER_TOKEN')
 
     test_message = 'hello world'
-    owner_id = '203517016'
+    owner_id = 203517016
     album_id = '280071703'
     photo = 'test_photo.jpg'
     from_group = 1  # 1 — запись будет опубликована от имени группы, 0 — запись будет опубликована от имени пользователя
 
-    # push_text_vk(vk_token, test_message, owner_id)
-    upload_image(vk_token, photo, album_id, owner_id)
+    image_id = upload_image_alboum(vk_token, photo, album_id, owner_id)
+    upload_post_to_wall(vk_token, test_message, owner_id, image_id)
