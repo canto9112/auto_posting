42c7f2219060c989d1ead2302104476c7ee898f3
canto9112 <cantoweb112@gmail.com>
2021-03-24 18:24:44 +0300

    Новая функция - постинг на стену группы в ВК

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from environs import Env
+import vk_api
+
+
+def push_text_vk(token, text, owner_id):
+    vk_session = vk_api.VkApi(token=token)
+
+    vk = vk_session.get_api()
+    vk.wall.post(message=text, owner_id=owner_id, from_group=1)
 
 
 if __name__ == '__main__':
@@ -6,3 +14,7 @@ if __name__ == '__main__':
     env.read_env()
 
     vk_token = env('VK_TOKEN')
+    test_message = 'hello world'
+    owner_id = '-203517016'
+    from_group = 1  # 1 — запись будет опубликована от имени группы, 0 — запись будет опубликована от имени пользователя
+    push_text_vk(vk_token, test_message, owner_id)
