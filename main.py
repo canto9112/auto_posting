828c72c827561e0fb6f7a2863e0b78899cef9d67
canto9112 <cantoweb112@gmail.com>
2021-03-27 11:18:37 +0300

    Собрал всех ботов в одном файле

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
+import vk_bot
+import telegram_bot
+import facebook_bot
 from environs import Env
-import vk_api
-from pprint import pprint
 
 
-def upload_image_alboum(token, photo, album_id, owner_id):
-    vk_session = vk_api.VkApi(token=token)
-    upload = vk_api.VkUpload(vk_session)
-
-    photo_img = upload.photo(photo,
-                             album_id=album_id,
-                             group_id=owner_id)
-    image_id = photo_img[0]['id']
-    return image_id
-
-
-def upload_post_to_wall(token, text, owner_id, media_id):
-    vk_session = vk_api.VkApi(token=token)
-
-    vk = vk_session.get_api()
-    vk.wall.post(message=text,
-                 owner_id=-owner_id,
-                 from_group=1,
-                 attachments=f'photo{-owner_id}_{media_id}')
-
-
-if __name__ == '__main__':
+def main():
     env = Env()
     env.read_env()
 
     vk_token = env('VK_USER_TOKEN')
+    telegram_token = env('TELEGRAM_BOT_TOKEN')
+    facebook_token = env('FACEBOOK_TOKEN')
 
-    test_message = 'hello world'
-    owner_id = 203517016
-    album_id = '280071703'
+    test_message = 'Тест 2345'
     photo = 'test_photo.jpg'
-    from_group = 1  # 1 — запись будет опубликована от имени группы, 0 — запись будет опубликована от имени пользователя
 
-    image_id = upload_image_alboum(vk_token, photo, album_id, owner_id)
-    upload_post_to_wall(vk_token, test_message, owner_id, image_id)
+    vk_owner_id = 203517016
+    vk_album_id = '280071703'
+    telegram_chanal_name = '@auto_posting_vk_tg'
+    facebook_group_id = 472212560631956
+
+    vk_bot.upload_image_alboum(vk_token, photo, vk_album_id, vk_owner_id, test_message)
+    telegram_bot.upload_post_to_chanal(telegram_token, telegram_chanal_name, test_message, photo)
+    facebook_bot.posting_post(facebook_token, test_message, facebook_group_id, photo)
+
+
+if __name__ == '__main__':
+    main()
