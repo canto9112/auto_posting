7ce62f61665234bd41d0e25285f4c5e06efa25e3
canto9112 <cantoweb112@gmail.com>
2021-03-31 17:48:54 +0300

    Переименовал переменные

--- google_sheets.py
+++ google_sheets.py
@@ -3,12 +3,12 @@ import requests
 
 
 def get_text_and_imagename(post_number, config_file, sheet_id):
-  gc = gspread.service_account(filename=config_file)
-  sh = gc.open_by_key(sheet_id)
+  service_account = gspread.service_account(filename=config_file)
+  spred_sheet = service_account.open_by_key(sheet_id)
 
-  worksheet = sh.sheet1
-  result3 = worksheet.row_values(post_number)
-  text,  url_image = result3
+  worksheet = spred_sheet.sheet1
+  result = worksheet.row_values(post_number)
+  text,  url_image = result
 
   response = requests.get(url_image)
   image_name = f'pic{post_number}.jpg'
