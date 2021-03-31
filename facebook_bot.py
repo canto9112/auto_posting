076e3587cbc31e49aabef888ff50a456f2cd2c80
canto9112 <cantoweb112@gmail.com>
2021-03-31 19:15:43 +0300

    Добавил надежности к открытию файла

--- facebook_bot.py
+++ facebook_bot.py
@@ -3,7 +3,10 @@ import requests
 
 def posting_post(token, text, group_id, image):
     url = f'https://graph.facebook.com/v10.0/{group_id}/photos'
-    files = {'file': open(image, 'rb')}
+    with open(image, "rb") as file:
+        image_file = file.read()
+    files = {'file': image_file}
+
     data = {
         "message": text,
         'access_token': token
