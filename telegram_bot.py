6d545953f897e5d2241ca5f70e298c46fb4057f6
canto9112 <cantoweb112@gmail.com>
2021-03-31 19:15:46 +0300

    Добавил надежности к открытию файла

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,10 @@ import telegram
 
 
 def upload_post_to_chanal(token, chanal_name, text, image):
+    with open(image, "rb") as file:
+        image_file = file.read()
+
     bot = telegram.Bot(token=token)
-    bot.send_photo(chat_id=chanal_name, photo=open(image, 'rb'))
+    bot.send_photo(chat_id=chanal_name, photo=image_file)
     bot.send_message(chat_id=chanal_name, text=text)
 
